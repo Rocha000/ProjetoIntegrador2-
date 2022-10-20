5cec77a09960c4e885fcf824c79cad59a79d7696
LuanMagriTarran <104510650+LuanMagriTarran@users.noreply.github.com>
2022-10-19 22:11:09 -0300

    Alterações JavaScript e Node
    Adicionamos o NodeJS e alteramos o JavaScript, agora ele remove o código do bilhete ao gerar um novo

--- Codigo/scripts/scriptTelaInicial.js
+++ Codigo/scripts/scriptTelaInicial.js
@@ -1,12 +1,12 @@
 function gerarCodigo(){
     chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    cod = '';
+    cod = 'Seu código: ';
     for (i = 0; i < 8; i++) {
         cod += chars.charAt(Math.floor(Math.random() * chars.length));
     }
 
-    console.dir(cod);
     textNode = document.createTextNode(cod);
-    document.getElementById("res").appendChild(textNode);
+    document.getElementById("res").textContent = cod;
+    console.dir(cod);
     return cod;
 }
